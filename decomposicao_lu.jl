799f2a012691ac7f360aa6b7d236d91550ecc624
Hugo Folloni <hugofollogua07@gmail.com>
2023-09-04 18:42:43 -0700

    Update decomposicao_lu.jl

--- decomposicao_lu.jl
+++ decomposicao_lu.jl
@@ -1,3 +1,5 @@
+using LinearAlgebra
+
 # Dada uma matrix A_nxn, retorna uma matrix Lower e uma matrix Upper que, multiplicadas, resultam em A. Útil para pré-processar dados.
 function decomp_lu(A)
     ### Para cada iteração, pega a primeira coluna e seta as próximas como combinação linear. Salva as bases e os vetores em matrizes que serão multiplicadas.
@@ -12,4 +14,17 @@ function decomp_lu(A)
         U[i,:] = vector    
     end
     return L, U
+end
+
+function calc_inversa(L, U)
+    tamanho = size(L)[1]
+    inversa = zeros(tamanho, tamanho)
+    identidade = Matrix{Int}(I, tamanho, tamanho)
+    
+    for i in 1:tamanho
+        y = L\identidade[:,i]
+        x = U\y
+        inversa[:,i] = x
+    end
+    return inversa
 end
